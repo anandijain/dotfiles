eabb0c93be0e240478549f2d73bdf1fcefb3d1d6
anand jain <anandj@uchicago.edu>
2023-04-27 02:52:16 -0400

    fix keys path

--- startup.jl
+++ startup.jl
@@ -2,4 +2,4 @@ using Startup
 OhMyREPL.enable_autocomplete_brackets(false)
 
 ENV["PATH"] = "/usr/bin:/bin:/usr/sbin:/sbin:/opt/homebrew/bin:/Users/anand/.juliaup/bin:/usr/local/bin/:/Users/anand/.cargo/bin/"
-include("keys.jl");
+include(joinpath(homedir(), "dotfiles/keys.jl"));
