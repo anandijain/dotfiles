7ee49a90c386109b1763402d90f9e4e0ec123d64
anand jain <anandj@uchicago.edu>
2023-04-27 02:49:12 -0400

    switch to startup pkg as in PrecompTools

--- .zprofile
+++ .zprofile
@@ -3,17 +3,20 @@ export JULIA_NUM_THREADS=8
 alias j="julia"
 alias jp="julia --project"
 alias jno="julia --startup-file=no"
+alias jnop="julia --startup-file=no --project"
 alias ja="julia -i -e 'using Pkg; Pkg.activate(;temp=true)'"
 alias js="code /Users/anand/.julia/config/startup.jl"
-alias zprof="code ~/.zprofile"
 alias jd="cd ~/.julia/dev/"
 alias jdbg="JULIA_DEBUG=loading julia --startup=no --project"
 alias ju="juliaup"
 
-alias la="ls -la"
+
+alias la="exa -lah"
+alias dotfiles="code ~/dotfiles"
 
 alias matlab="/Applications/MATLAB_R2022b.app/bin/matlab -nosplash -nodesktop"
 export JL_MATLAB_PATH="/Applications/MATLAB_R2022b.app/bin/matlab"
+
 jg () {
 	jd 
 	julia -e '
--- config.fish
+++ config.fish
@@ -1,6 +1,9 @@
 fish_add_path "/Users/anand/.juliaup/bin"
 fish_add_path "/opt/homebrew/bin/"
 fish_add_path "/Users/anand/.cargo/bin/"
+
+source "keys.fish"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
@@ -8,16 +11,23 @@ export JULIA_NUM_THREADS=8
 alias j="julia"
 alias jp="julia --project"
 alias jno="julia --startup-file=no"
+alias jnop="julia --startup-file=no --project"
 alias ja="julia -i -e 'using Pkg; Pkg.activate(;temp=true)'"
 alias js="code /Users/anand/.julia/config/startup.jl"
-alias zprof="code ~/.zprofile"
+alias dotfiles="code $HOME/dotfiles"
 alias jd="cd ~/.julia/dev/"
 alias jdbg="JULIA_DEBUG=loading julia --startup=no --project"
 alias ju="juliaup"
 
-alias la="ls -la"
+alias ls="exa"
+alias la="exa -lah"
 alias pip="pip3.10"
 alias python="python3.10"
 alias e="nvim"
 alias matlab="/Applications/MATLAB_R2022b.app/bin/matlab -nosplash -nodesktop"
+alias c="cargo"
 
+function jg
+    julia -e "jg(\"$argv\")"
+    code "$HOME/.julia/dev/$argv/"
+end
